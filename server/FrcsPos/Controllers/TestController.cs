6841dfb8b75063a6f7164279309467e75be0e663
Rahul Chand <rahulchand.usp@gmail.com>
2025-08-07 11:49:21 +1200

    pull

---
+++ server/FrcsPos/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+using System; using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrcsPos.Interfaces;
+using FrcsPos.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrcsPos.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : BaseController
+    {
+
+        public TestController(
+            IConfiguration configuration,
+            ITokenService tokenService,
+            ILogger<TestController> logger // ✅ Use generic ILogger<T>
+        ) : base(configuration, tokenService, logger)
+        {
+        }
+
+        [HttpGet("ping")]
+        public async Task<IActionResult> Ping()
+        {
+            return Ok(ApiResponse<string>.Ok("pong", "req retrieved", logger: _logger));
+        }
+    }
+}
